3d0547481389cd275c6294ba3ff4303fed3fbb4e
charly <charly@HPC.syneltec>
2020-04-26 11:31:03 +0200

    Added error definition ERROR_CALL_NOT_IMPLEMENTED

--- src/NanoWinError.h
+++ src/NanoWinError.h
@@ -37,6 +37,7 @@
 #define ERROR_NOT_SUPPORTED            (0x32)    //    50 // The request is not supported
 #define ERROR_FILE_EXISTS              (0x50)    //    80 // The file exists
 #define ERROR_INVALID_PARAMETER        (0x57)    //    87 // The parameter is incorrect
+#define ERROR_CALL_NOT_IMPLEMENTED     (0x78)    //   120 // CCL from EinError.h
 #define ERROR_INSUFFICIENT_BUFFER      (0x7A)    //   122 // The data area passed to a system call is too small
 #define ERROR_ALREADY_EXISTS           (0XB7)    //   183 // Cannot create a file when that file already exists
 #define ERROR_ENVVAR_NOT_FOUND         (0XCB)    //   203 // The system could not find the environment option that was entered
